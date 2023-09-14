3ce447b28a2cb8b2ddd9c8fa1b2c5574deae14c6
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-09-14 22:21:33 +0800

    chore: change config loading priority

--- config/init.go
+++ config/init.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hitokoto-osc/notification-worker/logging"
 	viper "github.com/spf13/viper"
 	"go.uber.org/zap"
+	"strings"
 )
 
 func Init(path string) {
@@ -18,6 +19,7 @@ func Init(path string) {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")        // 二进制执行目录
 	viper.AddConfigPath("./config") // 二进制执行目录的配置文件夹
+	viper.AddConfigPath("./data")
 	viper.AddConfigPath("./bin")
 	viper.AddConfigPath("..")      // 二进制上级目录
 	viper.AddConfigPath("../conf") // 二进制上级目录的配置文件夹
@@ -30,6 +32,12 @@ func Init(path string) {
 		}
 		logger.Warn("未检测到配置文件，使用环境变量运行。")
 	}
+
+	// Parse env config
+	viper.SetEnvPrefix("notification_worker")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
 	logger.Debug("已成功加载配置。",
 		zap.String("config_file_used", viper.ConfigFileUsed()),
 		zap.Any("settings", viper.AllSettings()),
